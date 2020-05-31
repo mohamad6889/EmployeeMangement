85121a74b76840a1ec697e3242d9fd4e9a1815bd
Mohammad Amani <mohamad.amani68@gmail.com>
2020-05-31 18:30:45 +0430

    [edit form has been added]

--- src/main/java/com/employee/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/employee/controller/EmployeeController.java
@@ -1,10 +1,16 @@
 package com.employee.employee.controller;
 
+import com.employee.employee.model.Employee;
 import com.employee.employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
@@ -19,5 +25,25 @@ public class EmployeeController {
     public String viewHome(Model model) {
         model.addAttribute("employeeList", employeeService.getAllEmployees());
         return "home";
+    }
+
+    @GetMapping(path = "/newEmployeeForm")
+    public String newEmployeeForm(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "newEmployee";
+    }
+
+    @PostMapping(path = "/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.createEmployee(employee);
+        return "redirect:/";
+    }
+
+    @GetMapping(path = "/employeeEditForm/{id}")
+    public String editEmployeeForm(@PathVariable(value = "id") Long id, Model model) {
+        Employee employee = employeeService.getEmployeeById(id);
+        model.addAttribute("employee", employee);
+        return "editEmployee";
     }
 }
--- src/main/java/com/employee/employee/service/EmployeeService.java
+++ src/main/java/com/employee/employee/service/EmployeeService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
@@ -18,5 +19,20 @@ public class EmployeeService {
 
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    public void createEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    public Employee getEmployeeById(Long id) {
+        Optional <Employee> optionalEmployee = employeeRepository.findById(id);
+        Employee employee = null;
+        if (optionalEmployee.isPresent()) {
+            employee = optionalEmployee.get();
+        } else {
+            throw new RuntimeException("Employee's Id is Not Valid => " + id);
+        }
+        return employee;
     }
 }
