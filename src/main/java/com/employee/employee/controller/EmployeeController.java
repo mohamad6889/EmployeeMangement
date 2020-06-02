dbc043c00bf0aafd8050d74a55b234d616fc06fc
Mohammad Amani <mohamad.amani68@gmail.com>
2020-06-02 08:47:59 +0430

    pagination added

--- src/main/java/com/employee/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/employee/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.employee.employee.controller;
 import com.employee.employee.model.Employee;
 import com.employee.employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class EmployeeController {
@@ -23,8 +26,8 @@ public class EmployeeController {
 
     @GetMapping("/")
     public String viewHome(Model model) {
-        model.addAttribute("employeeList", employeeService.getAllEmployees());
-        return "home";
+        //model.addAttribute("employeeList", employeeService.getAllEmployees());
+        return findPaginated(1, model);
     }
 
     @GetMapping(path = "/newEmployeeForm")
@@ -45,5 +48,25 @@ public class EmployeeController {
         Employee employee = employeeService.getEmployeeById(id);
         model.addAttribute("employee", employee);
         return "editEmployee";
+    }
+
+    @GetMapping(path = "/deleteEmployee/{id}")
+    public String deleteEmployee(@PathVariable(value = "id") Long id) {
+        employeeService.deleteEmployee(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/page/{pageNumber}")
+    public String findPaginated(@PathVariable(value = "pageNumber") int pageNumber, Model model) {
+        int pageSize = 5;
+
+        Page< Employee > page = employeeService.findAllWithPagination(pageNumber, pageSize);
+        List< Employee > employeesList = page.getContent();
+
+        model.addAttribute("currentPage", pageNumber);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalElements", page.getTotalElements());
+        model.addAttribute("employeesList", employeesList);
+        return "home";
     }
 }
--- src/main/java/com/employee/employee/service/EmployeeService.java
+++ src/main/java/com/employee/employee/service/EmployeeService.java
@@ -3,6 +3,10 @@ package com.employee.employee.service;
 import com.employee.employee.model.Employee;
 import com.employee.employee.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,29 +14,38 @@ import java.util.Optional;
 
 @Service
 public class EmployeeService {
-    private EmployeeRepository employeeRepository;
+  private EmployeeRepository employeeRepository;
 
-    @Autowired
-    public EmployeeService(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
-    }
+  @Autowired
+  public EmployeeService(EmployeeRepository employeeRepository) {
+    this.employeeRepository = employeeRepository;
+  }
 
-    public List<Employee> getAllEmployees() {
-        return employeeRepository.findAll();
-    }
+  public List<Employee> getAllEmployees() {
+    return employeeRepository.findAll();
+  }
 
-    public void createEmployee(Employee employee) {
-        employeeRepository.save(employee);
-    }
+  public void createEmployee(Employee employee) {
+    employeeRepository.save(employee);
+  }
 
-    public Employee getEmployeeById(Long id) {
-        Optional <Employee> optionalEmployee = employeeRepository.findById(id);
-        Employee employee = null;
-        if (optionalEmployee.isPresent()) {
-            employee = optionalEmployee.get();
-        } else {
-            throw new RuntimeException("Employee's Id is Not Valid => " + id);
-        }
-        return employee;
+  public Employee getEmployeeById(Long id) {
+    Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+    Employee employee = null;
+    if (optionalEmployee.isPresent()) {
+      employee = optionalEmployee.get();
+    } else {
+      throw new RuntimeException("Employee's Id is Not Valid => " + id);
     }
+    return employee;
+  }
+
+  public void deleteEmployee(Long id) {
+    employeeRepository.deleteById(id);
+  }
+
+  public Page<Employee> findAllWithPagination(int pageNumber, int pageSize) {
+    Pageable pageable = PageRequest.of(pageNumber - 1, pageSize);
+    return employeeRepository.findAll(pageable);
+  }
 }
